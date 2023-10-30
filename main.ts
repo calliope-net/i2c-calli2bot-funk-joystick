57684f313375e0c43c88e8c68512b4f6eeae25eb
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-10-30 19:59:59 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    Calli2bot.fahreJoystick(receivedNumber)
+})
+let Calli2bot: calli2bot.Calli2bot = null
+radio.setGroup(22)
+Calli2bot = calli2bot.beimStart(calli2bot.eADDR.CB2_x22)
+lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
