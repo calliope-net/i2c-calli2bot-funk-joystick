74d434fa1ad9f7d82d31a21913d79d05d1681497
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-10-30 21:57:04 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 radio.onReceivedNumber(function (receivedNumber) {
     Calli2bot.fahreJoystick(receivedNumber)
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, lcd16x2rgb.lcd16x2_text(Calli2bot.getLog()[0]))
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 15, lcd16x2rgb.lcd16x2_text(Calli2bot.getLog()[1]))
 })
 let Calli2bot: calli2bot.Calli2bot = null
 radio.setGroup(22)
